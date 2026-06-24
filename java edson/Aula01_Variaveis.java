89fb87b835bf9aa422f66e4e1aebe675fc1ea606
caio-ohya <caio.e.lima@escola.pr.gov.br>
2026-06-24 09:52:39 -0300

    Add files via upload

---
+++ java edson/Aula01_Variaveis.java
@@ -0,0 +1,17 @@
+package CursoJava;
+
+public class Aula01_Variaveis {
+
+    public static void main(String[] args){
+    int idade = 17;
+    double altura = 1.75;
+    boolean matriculado = true;
+    String nome = "João Kudlax";
+
+    System.out.println("Nome:" + nome);
+    System.out.println("Idade:" + idade + "anos");
+    System.out.println("Altura:" + altura + "m");
+    System.out.println("Aluno matriculado? :" + matriculado);
+
+};     
+};
